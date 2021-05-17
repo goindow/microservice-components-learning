867f1f67e8e8d4324112b12b094ab5a2923e3b3a
hyb <76788424@qq.com>
2021-05-17 14:45:23 +0800

    add annotation@EnableDiscoveryClient for index

--- microservice-discovery/microservice-discovery-nacos/src/main/java/indi/goindow/demo/App.java
+++ microservice-discovery/microservice-discovery-nacos/src/main/java/indi/goindow/demo/App.java
@@ -2,9 +2,11 @@ package indi.goindow.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 @RequestMapping("/user")
 public class App {
